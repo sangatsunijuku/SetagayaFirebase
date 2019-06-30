b5c5d4efdf974cb19ff820a5f2a887a9cbbfb58b
“sangatsunijuku” <yusukekubotokyo@gmil.com>
2019-07-01 02:00:57 +0900

    Initial Commit

---
+++ SetagayaFirebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetagayaFirebase
+//
+//  Created by sangatsunijuku on 2019/07/01.
+//  Copyright © 2019 sangatsunijuku. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
